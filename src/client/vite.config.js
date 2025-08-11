c427b7c8ee2e13146e6749e937f1465250aeafbe
a2v-stg <aavelliangiri@thesummitgrp.com>
2025-08-11 20:13:04 +0530

    chore: fix vite config

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -16,13 +16,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace(/^\/api/, '/api'),
       },
       '/auth': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace(/^\/auth/, '/auth'),
       },
     },
   },
